40d7a3372056104a9952082b755242e00553ac29
Marcel Dragon <dragon.marcel@o2.pl>
2023-02-07 08:19:22 +0100

    init recruitment task

---
+++ src/main/java/com/example/Asseco/AssecoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Asseco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssecoApplication.class, args);
+	}
+
+}
